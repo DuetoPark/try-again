fbd433507dbf26d98909dba6916ae26cb310c053
DuetoPark <inhye.park.dev@gmail.com>
2021-12-27 01:15:58 +0900

    🚧 Import tiny-slider.js to the project and initailize productCarousel

---
+++ js/_carousel.js
@@ -0,0 +1,12 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  navContainer: '.product-carousel .thumbnail-list',
+  controls: false,
+  navAsThumbnails: true,
+  arrowKeys: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: 'auto',
+})
