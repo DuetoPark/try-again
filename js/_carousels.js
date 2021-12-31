fde420ad20ac2091edecb7ee1a8ae2ec55dac53b
DuetoPark <inhye.park.dev@gmail.com>
2021-12-31 15:58:38 +0900

    ♻️ Refactor _carousels.js

--- js/_carousels.js
+++ js/_carousels.js
@@ -1,38 +1,36 @@
+const commonOptions = {
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: 'auto',
+}
+
 const productCarousel = tns({
+  ...commonOptions,
   container: '.product-carousel .slider-list',
   navContainer: '.product-carousel .thumbnail-list',
   controls: false,
-  navAsThumbnails: true,
-  arrowKeys: true,
   autoplay: true,
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
-  mouseDrag: true,
-  preventScrollOnTouch: 'auto',
 })
 
 const userGalleryMobile = tns({
+  ...commonOptions,
   container: '.user-gallery.is-mobile .slider-list',
   navContainer: '.user-gallery.is-mobile .thumbnail-list',
   controls: false,
   loop: false,
-  navAsThumbnails: true,
-  arrowKeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: 'auto',
   gutter: 4,
   edgePadding: 16,
 })
 
 const userGalleryTablet = tns({
+  ...commonOptions,
   container: '.user-gallery.is-tablet .slider-list',
   navContainer: '.user-gallery.is-tablet .thumbnail-list',
   controlsContainer: '.user-gallery.is-tablet .user-gallery-controls',
   loop: false,
-  navAsThumbnails: true,
-  arrowKeys: true,
-  mouseDrag: true,
-  preventScrollOnTouch: 'auto',
   gutter: 6,
   edgePadding: 80,
 })
