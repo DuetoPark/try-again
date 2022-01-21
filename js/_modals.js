92f813f728b5f931eba5ff90901594c0e28ff201
DuetoPark <inhye.park.dev@gmail.com>
2022-01-21 21:16:16 +0900

    ♻️ Refactor _modal.js: class → function

--- js/_modals.js
+++ js/_modals.js
@@ -1,105 +1,62 @@
-// NOTE: Modal
-class Modal {
-  constructor(target, trigger, closeButton) {
-    this.modal = document.querySelector(target);
-    this.overlay = document.querySelector('.overlay');
-    this.trigger = document.querySelector(trigger) || null;
-    this.triggers = document.querySelectorAll(trigger) || null;
-    this.closeButton = this.modal.querySelector(closeButton) || null;
-    this.closeButtons = this.modal.querySelectorAll(closeButton) || null;
-  }
+const overlay = document.querySelector('.overlay');
+let isOpened = new Set();
+let triggers = [];
+let closeTriggers = [];
+const idArr = [
+  'sidebar',
+  'order-form-modal',
+  'add-to-cart-modal',
+  'search-modal',
+  'search-history',
+  'my-menu',
+  'toast',
+];
 
-  active() {
-    this.modal.classList.add('is-active');
-    this.overlay.classList.add('is-active');
-  }
+function triggerBtns(id) {
+  return document.querySelectorAll(`button[data-id="${id}"]`);
+}
 
-  inactive() {
-    this.modal.classList.remove('is-active');
-    this.overlay.classList.remove('is-active');
-  }
+function triggerInputs(id) {
+  return document.querySelectorAll(`input[data-id="${id}"]`);
+}
 
-  addEvent() {
-    this.trigger?.addEventListener('click', this.active.bind(this));
-    this.triggers?.forEach((trigger) => {
-      trigger.addEventListener('click', this.active.bind(this));
-    });
+function onClose() {
+  overlay.classList.remove('is-active');
 
-    this.overlay.addEventListener('click', this.inactive.bind(this));
-    this.closeButton?.addEventListener('click', this.inactive.bind(this));
-    this.closeButtons?.forEach((button) =>
-      button.addEventListener('click', this.inactive.bind(this))
-    );
+  isOpened.forEach((modal) => modal.classList.remove('is-active'));
+  isOpened.clear();
+}
+
+function onOpen(event, id) {
+  const modal = document.querySelector(`.${id}[data-id="${id}"]`);
+  modal.classList.add('is-active');
+  isOpened.add(modal);
+
+  if (event.type === 'click') {
+    overlay.classList.add('is-active');
   }
 }
 
-const sidebar = new Modal('.sidebar', '.gnb-icon-button.is-menu');
-sidebar.addEvent();
+window.addEventListener('load', () => {
+  const closeBtns = document.querySelectorAll('button.close-button');
 
-const orderFormModal = new Modal('.order-form-modal', '.order-button');
-orderFormModal.addEvent();
+  idArr.forEach((id) => {
+    triggers = [...triggers, ...triggerBtns(id), ...triggerInputs(id)];
+  });
 
-const addToCartModal = new Modal(
-  '.add-to-cart-modal',
-  '.cart-button',
-  '.delete-button'
-);
-addToCartModal.addEvent();
+  triggers.forEach((trigger) => {
+    const id = trigger.dataset.id;
+    const except = trigger.classList.value.includes('close');
+    if (except) return;
 
-const searchModal = new Modal(
-  '.search-modal',
-  '.gnb-icon-button.is-search',
-  '.close-button'
-);
-searchModal.addEvent();
+    if (id === 'search-history' || id === 'my-menu') {
+      trigger.addEventListener('focus', (e) => onOpen(e, id));
+      trigger.addEventListener('blur', onClose);
+    } else {
+      trigger.addEventListener('click', (e) => onOpen(e, id));
+    }
+  });
 
-// NOTE: Toast
-class Toast extends Modal {
-  active() {
-    this.modal.classList.add('is-active');
-  }
-
-  inactive() {
-    this.modal.classList.remove('is-active');
-  }
-
-  addEvent() {
-    this.trigger?.addEventListener('click', this.active.bind(this));
-    this.triggers?.forEach((trigger) => {
-      trigger.addEventListener('click', this.active.bind(this));
-    });
-
-    this.closeButton?.addEventListener('click', this.inactive.bind(this));
-    this.closeButtons?.forEach((button) =>
-      button.addEventListener('click', this.inactive.bind(this))
-    );
-  }
-}
-
-const toast = new Toast('.toast', '.bookmark-button', '.delete-button');
-toast.addEvent();
-
-// NOTE: NoOverlayModal
-class NoOverlayModal extends Modal {
-  active() {
-    this.modal.classList.add('is-active');
-  }
-
-  inactive() {
-    this.modal.classList.remove('is-active');
-  }
-
-  addEvent() {
-    this.trigger.addEventListener('focus', this.active.bind(this));
-    this.trigger.addEventListener('blur', this.inactive.bind(this));
-  }
-}
-
-const gnbSearchHistory = new NoOverlayModal(
-  '.gnb-search .search-history',
-  '.gnb-search .form-input'
-);
-gnbSearchHistory.addEvent();
-
-const gnbMyMenu = new NoOverlayModal('.my-menu', '.my-menu-button');
-gnbMyMenu.addEvent();
+  closeTriggers = [...closeBtns, overlay];
+  closeTriggers.forEach((btn) => btn.addEventListener('click', onClose));
+});
