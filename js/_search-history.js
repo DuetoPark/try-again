e3b875098cc990008e865264cbc9f02d82fcaf47
DuetoPark <inhye.park.dev@gmail.com>
2022-02-06 22:52:33 +0900

    ♻️ Refactor _search-history.js

--- js/_search-history.js
+++ js/_search-history.js
@@ -5,8 +5,8 @@ export default class History {
     this.form.addEventListener('submit', this.onSubmit);
 
     this.input = this.form.querySelector('.form-input');
-    this.history = this.section.querySelector('.search-history-list');
-    this.history.addEventListener('click', this.onHistoryClick);
+    this.list = this.section.querySelector('.search-history-list');
+    this.list.addEventListener('click', this.onHistoryClick);
 
     this.clearBtn = this.section.querySelector('.clear-button');
     this.clearBtn.addEventListener('click', this.onClearBtnClick);
@@ -18,33 +18,13 @@ export default class History {
     this.callback = callback;
   }
 
-  getTemplate(id) {
-    const text = this.input.value;
-
-    let template = `
-          <button class="word-button" type="button">
-            ${text}
-          </button>
-          <button
-            class="delete-button"
-            type="button"
-            aria-label="검색어 삭제"
-            data-id="${id}"
-          >
-            <i class="ic-close" aria-hidden="true"></i>
-          </button>
-        `;
-
-    return template;
-  }
-
   onClearBtnClick = () => {
     this.clear();
     this.init();
   };
 
   clear() {
-    this.history.innerHTML = '';
+    this.list.innerHTML = '';
   }
 
   onHistoryClick = (event) => {
@@ -55,7 +35,7 @@ export default class History {
   };
 
   removeItem(id) {
-    const item = this.history.querySelector(`li[data-id="${id}"]`);
+    const item = this.list.querySelector(`li[data-id="${id}"]`);
     item.remove();
   }
 
@@ -65,33 +45,44 @@ export default class History {
     const text = this.input.value;
     if (!text) return;
 
-    const li = this.createItem();
-    this.addItem(li);
-    this.init();
+    if (this.list.firstElementChild?.matches('.placeholder')) {
+      this.clear();
+    }
+
+    this.addItem(text);
     this.keep5Items();
+    this.init();
   };
 
   keep5Items() {
-    const countChildren = this.history.childElementCount;
+    const countChildren = this.list.childElementCount;
     if (countChildren <= this.itemCountLimit) return;
 
-    const lastChild = this.history.lastElementChild;
+    const lastChild = this.list.lastElementChild;
     lastChild.remove();
   }
 
-  addItem(elem) {
-    const first = this.history.firstElementChild;
-    this.history.insertBefore(elem, first);
-  }
-
-  createItem() {
+  addItem(text) {
     const id = uuidv4();
 
     const li = document.createElement('li');
     li.setAttribute('class', 'search-history-item');
     li.setAttribute('data-id', id);
-    li.innerHTML = this.getTemplate(id);
-    return li;
+    li.innerHTML = `
+      <button class="word-button" type="button">
+        ${text}
+      </button>
+      <button
+        class="delete-button"
+        type="button"
+        aria-label="검색어 삭제"
+        data-id="${id}"
+      >
+        <i class="ic-close" aria-hidden="true"></i>
+      </button>`;
+
+    const firstItem = this.list.firstElementChild;
+    this.list.insertBefore(li, firstItem);
   }
 
   init() {
