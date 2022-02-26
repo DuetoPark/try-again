9b8cbbbd314bbd05d97289a62e437c499f5a37e5
DuetoPark <inhye.park.dev@gmail.com>
2022-02-26 14:29:46 +0900

    ♻️ Refactor .form-checkout.js

--- js/form-checkout.js
+++ js/form-checkout.js
@@ -1,53 +1,63 @@
 import {
   PRICE_PRODUCT,
   PRICE_EXTRA,
-  selectedOption,
+  currentOption,
   generatedItem,
   userSelect,
   updateTotalAmount,
+  formCheckoutList,
 } from './form.js'
 
-const orderFormCheckoutList = document.querySelectorAll('.order-checkouts')
-
-function deleteCheckoutListItem(event) {
+function deleteCheckoutItem(event) {
   const target = event.target
 
   if (!target.matches('.delete-button')) return
 
   const key = event.target.dataset.key
-  generatedItem[key].forEach((item) => item.remove())
+  generatedItem[key].forEach((item) => {
+    const parent = item.parentNode
+    parent.removeChild(item)
+  })
+
   generatedItem[key] = []
 
-  selectedOption.delete(key)
+  currentOption.delete(key)
   userSelect.set(key, 0)
 
   updateTotalAmount()
 }
 
-function updateCheckoutAmount(event) {
+function updateCheckoutCard(event) {
   const target = event.target
-  const id = target.id
   const [key, num] = target.value.split('-')
 
   userSelect.set(key, Number(num))
 
-  const cardOutputList = document.querySelectorAll(
-    `output[for="${id}"] .amount`
-  )
-  cardOutputList.forEach((output) => {
-    const cardPrice = (key === 'mandarin' ? PRICE_EXTRA : PRICE_PRODUCT) * num
-    output.textContent = cardPrice.toLocaleString()
-  })
-
-  const cardSelectList = document.querySelectorAll(`select[id="${id}"]`)
-  cardSelectList.forEach((select) => {
-    select.options[num - 1].selected = true
-  })
-
+  updateCardSelect(target)
+  updateCardAmount(target)
   updateTotalAmount()
 }
 
-orderFormCheckoutList.forEach((checkout) => {
-  checkout.addEventListener('click', deleteCheckoutListItem)
-  checkout.addEventListener('change', updateCheckoutAmount)
+function updateCardSelect(target) {
+  const [key, num] = target.value.split('-')
+
+  generatedItem[key].forEach((item) => {
+    const select = item.querySelector('select')
+    select.options[num - 1].selected = true
+  })
+}
+
+function updateCardAmount(target) {
+  const [key, num] = target.value.split('-')
+
+  generatedItem[key].forEach((item) => {
+    const amount = item.querySelector('.amount')
+    const cardPrice = (key === 'mandarin' ? PRICE_EXTRA : PRICE_PRODUCT) * num
+    amount.textContent = cardPrice.toLocaleString()
+  })
+}
+
+formCheckoutList.forEach((checkout) => {
+  checkout.addEventListener('click', deleteCheckoutItem)
+  checkout.addEventListener('change', updateCheckoutCard)
 })
